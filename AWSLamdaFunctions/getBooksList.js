5a108862784616a8647865619460265189cc4b88
Anu Parmar <anuparmar@Anus-MacBook-Pro.local>
2021-04-16 17:36:02 -0700

    Added 4 lamda Functions

---
+++ AWSLamdaFunctions/getBooksList.js
@@ -0,0 +1,43 @@
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({region: "us-east-1"});
+
+
+exports.handler = (event, context, callback) => {
+
+    const getparams = {
+        TableName: "books",
+        FilterExpression: '#expressionValue = :expressionName',
+        ExpressionAttributeNames: {
+            '#expressionValue': 'active',
+        },
+        ExpressionAttributeValues: {
+            ':expressionName': true,
+        },
+    };
+
+
+    docClient.scan(getparams, onScan);
+
+    function onScan(err, data) {
+
+        if (err) {
+            console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+        }
+
+        const response = {
+            statusCode: 200,
+            headers: {
+                "Content-Type" : "application/json",
+                "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+                "Access-Control-Allow-Methods" : "OPTIONS,POST",
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+                "X-Requested-With" : "*"
+            },
+            body: data,
+        };
+
+        callback(null, response);
+    }
+
+};
