16a650df0f694e155df7577cf1e9c091e5bbbc46
Anu Parmar <anuparmar@Anus-MacBook-Pro.local>
2021-05-04 22:10:15 -0700

    Lambda Functions Updated

--- AWSLamdaFunctions/adduser.js
+++ AWSLamdaFunctions/adduser.js
@@ -12,55 +12,81 @@ exports.handler = (event, context, callback) => {
     docClient.scan(getparams, onScan);
 
     function onScan(err, data) {
-        if (err) {
-            console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
-        } else {
-            console.log("Scan succeeded.");
+
+        if(event.username)
+        {   var found =false;
             data.Items.forEach(function(users) {
-                console.log("displaying ID"+count);
-                count++;
-
+                if(event.username===users.username)
+                {   found=true; }
             });
-
-            count = count + 1 ;
-        }
-        console.log("id in D in on scanB" + count);
-
-        const params = {
-            Item: {
-                id : count,
-                username: event.username,
-                firstname: event.firstname,
-                lastname: event.lastname,
-                email: event.email,
-                password : event.password,
-                role : event.role
-            },
-            TableName: "users"
-        };
-        const response = {
-            statusCode: 200,
-            headers: {
-                "Content-Type" : "application/json",
-                "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-                "Access-Control-Allow-Methods" : "OPTIONS,POST",
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': true,
-                "X-Requested-With" : "*"
-            },
-            body: JSON.stringify('User added Successfully'),
-        };
-
-        docClient.put(params, function(err, data) {
-            if(err){
-                callback(err, null);
-            } else {
-                callback(null, data);
+            if(found)
+            {
+                const response = {
+                    statusCode: 200,
+                    headers: {
+                        "Content-Type" : "application/json",
+                        "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+                        "Access-Control-Allow-Methods" : "OPTIONS,POST",
+                        "Access-Control-Allow-Origin": '*',
+                        "Access-Control-Allow-Credentials": true,
+                        "X-Requested-With" : "*"
+                    },
+                    body: JSON.stringify('user already exist'),
+                };
+                callback(null, response);
             }
-        });
-        callback(null, response);
-    }
-    console.log("id in DB" + global.count);
+            else{
+                if (err) {
+                    console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+                } else {
+                    console.log("Scan succeeded.");
+                    data.Items.forEach(function(users) {
+                        console.log("displaying ID"+count);
+                        count++;
 
+                    });
+
+                    count = count + 1 ;
+                }
+                console.log("id in D in on scanB" + count);
+
+                const params = {
+                    Item: {
+                        id : count,
+                        username: event.username,
+                        firstname: event.firstname,
+                        lastname: event.lastname,
+                        email: event.email,
+                        password : event.password,
+                        role : event.role,
+                        address : event.address + " " + event.pincode
+                    },
+                    TableName: "users"
+                };
+                const response = {
+                    statusCode: 200,
+                    headers: {
+                        "Content-Type" : "application/json",
+                        "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+                        "Access-Control-Allow-Methods" : "OPTIONS,POST",
+                        "Access-Control-Allow-Origin": '*',
+                        "Access-Control-Allow-Credentials": true,
+                        "X-Requested-With" : "*"
+                    },
+                    body: JSON.stringify('User added Successfully'),
+                };
+
+                docClient.put(params, function(err, data) {
+                    if(err){
+                        callback(err, null);
+                    } else {
+                        callback(null, data);
+                    }
+                });
+                callback(null, response);
+            }
+        }
+
+    }
 
 };
